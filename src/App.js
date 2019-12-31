ebaf39357f8cd012829b484953e4fb922f15c0ef
Johnny Oshika <johnnyoshika@gmail.com>
2019-12-31 08:55:44 -0800

    Use axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import axios from 'axios';
+
+const axiosGitHubGraphQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+  }
+});
 
 const TITLE = 'React GraphQL GitHub Client';
 
